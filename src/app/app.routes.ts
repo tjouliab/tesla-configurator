047019c35f5b31574203e577dbfa820efaae7959
Tim <tim.jouliab@icloud.com>
2024-02-17 17:23:16 +0100

    feat: implementing routing and creation of the steps components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StepOneComponent } from './steps/step-one/step-one.component';
+import { StepTwoComponent } from './steps/step-two/step-two.component';
+import { StepThreeComponent } from './steps/step-three/step-three.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'step1', component: StepOneComponent },
+  { path: 'step2', component: StepTwoComponent },
+  { path: 'step3', component: StepThreeComponent },
+];
---
+++ src/app/steps/step-two/step-two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-two',
+  standalone: true,
+  imports: [],
+  templateUrl: './step-two.component.html',
+  styleUrl: './step-two.component.scss'
+})
+export class StepTwoComponent {
+
+}
